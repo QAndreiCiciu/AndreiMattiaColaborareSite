3e83adfc6e38426a4bfcee4336e31ee5141ba002
QAndreiCiciu <andreiciciu@gmail.com>
2026-04-21 09:48:49 +0300

    Initial Commit v2

---
+++ main.js
@@ -0,0 +1,12 @@
+var num = 0
+function color(){
+    num ++
+    alert("Mattia s-a cacat pe el de " + num + " ori")
+    if(num==20){
+        alert("Mattia are ibs")
+    }
+}
+function flush(){
+    num=0;
+    alert("Ai tras apa")
+}
